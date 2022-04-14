654bdcf0498d3e36fb461482268d7b1d9b529928
Kumar <mijarkumar2019@gmail.com>
2022-04-14 07:31:36 +0545

    Add Data Types

---
+++ Classes/main/FirstEntryCode.cs
@@ -0,0 +1,41 @@
+// See https://aka.ms/new-console-template for more information
+
+//using system;
+
+namespace ABC
+{
+
+class EntryPoint{
+    public static void Main(){
+        //This is single line comment
+        /*
+        This is multiple line comment.
+        */
+    
+    //Pascal Casing:method and class
+    //Camel Casing:
+    Console.Write("Hey Kumar!!");
+    
+
+
+//Data types
+//Number, integer, floating point
+//byte a=24 =255;
+//short b=334; //16-bit
+//int  x=55213;
+//long d=1111122222233333;
+
+//floating e=467.232f;
+//double f=
+//decimal g=
+
+//bool h= 
+//char i='d'; 
+//string j = "Deep";
+//object 0=" ";
+
+
+
+}
+}
+}
